ae5a4fe9acd8acba54a2d0f7731f95aaa2618c06
cyr <cyr@zolotarev.pp.ua>
2020-07-26 15:50:55 +0300

    #004 sum of primes

--- easy/004-sum-of-primes/sum_of_primes.py
+++ easy/004-sum-of-primes/sum_of_primes.py
@@ -1,11 +1,11 @@
 def is_prime(n):
     if n == 2 or n == 3:
         return True
-    if n < 2 or n % 2 == 0:
+    if n < 2 or not n % 2:
         return False
     if n < 9:
         return True
-    if n % 3 == 0:
+    if not n % 3:
         return False
     r = int(n ** 0.5)
     f = 5
