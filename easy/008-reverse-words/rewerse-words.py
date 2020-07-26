4bcc9307321a9e7f641126da4226ac4639e3982b
cyr <cyr@zolotarev.pp.ua>
2020-07-26 15:58:13 +0300

    #008 reverse words

---
+++ easy/008-reverse-words/rewerse-words.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = "input.txt"
+
+    f = open(filename, 'r')
+    for i in f:
+        print(*i.split()[::-1])
+    f.close()
