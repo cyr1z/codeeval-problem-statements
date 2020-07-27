a32ada0e56198033acc614e2ab17397ca78df944
cyr <cyr@zolotarev.pp.ua>
2020-07-27 21:29:31 +0300

    #092 penultimate word

---
+++ easy/092-penultimate-word/penultimate_word.py
@@ -0,0 +1,12 @@
+import sys
+
+if __name__ == '__main__':
+    filename = "input.txt"
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+
+    f = open(filename, 'r')
+    for line in f:
+        line_list = list(line.strip().split())
+        print(line_list[-2])
+    f.close()
