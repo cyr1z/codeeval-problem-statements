c0b864c5b6d644fecddf286b6ccd596548d55115
cyr <cyr@zolotarev.pp.ua>
2020-08-02 20:05:14 +0300

    #116 morse code refactoring and test

--- easy/116-morse-code/morse_code.py
+++ easy/116-morse-code/morse_code.py
@@ -51,8 +51,45 @@ morse_code = {
 morse_code_revert = dict((val, key.upper()) for key, val in morse_code.items())
 
 
-def de_morse(x):
-    return ''.join(morse_code_revert[i] for i in x)
+def morse_word(x):
+    """
+    convert an alphabet word to space separated word in morse code
+    :param x: str word
+    :return: str space separated word in morse code
+    """
+    d = [x.lower() for x in list(x)]
+    return ' '.join(morse_code[i] for i in d)
+
+
+def morse_string(x):
+    """
+    convert an alphabet string to double space separated word in morse code
+    :param x: str string
+    :return: str double space separated string in morse code
+    """
+    words_list = [x for x in x.strip().split()]
+    words = list(map(morse_word, words_list))
+    return '  '.join(words).strip()
+
+
+def de_morse_word(x):
+    """
+    convert a space separated word in morse code to alphabet word
+    :param x: str each letter separated by space char
+    :return: str word
+    """
+    return ''.join(morse_code_revert[i] for i in x.split())
+
+
+def de_morse_string(x):
+    """
+    convert a double space separated string in morse code to alphabet string
+    :param x: str each word separated by 2 space chars.
+    :return: str text string
+    """
+    words_list = [x for x in x.strip().split('  ')]
+    words = list(map(de_morse_word, words_list))
+    return ' '.join(words).strip()
 
 
 if __name__ == '__main__':
@@ -62,6 +99,5 @@ if __name__ == '__main__':
 
     with open(filename, "r") as read_file:
         for line in read_file:
-            words_list = [list(x.split()) for x in line.strip().split('  ')]
-            words = list(map(de_morse, words_list))
-            print(' '.join(words))
+            print(de_morse_string(line))
+            # print(morse_string(de_morse_string(line)) == line.strip())
