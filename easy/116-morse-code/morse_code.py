94511b2ab889006d29247f1825c047da2c998525
cyr <cyr@zolotarev.pp.ua>
2020-08-02 18:12:31 +0300

    #116 morse code

---
+++ easy/116-morse-code/morse_code.py
@@ -0,0 +1,67 @@
+import sys
+
+morse_code = {
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}
+
+morse_code_revert = dict((val, key.upper()) for key, val in morse_code.items())
+
+
+def de_morse(x):
+    return ''.join(morse_code_revert[i] for i in x)
+
+
+if __name__ == '__main__':
+    filename = "input.txt"
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+
+    with open(filename, "r") as read_file:
+        for line in read_file:
+            words_list = [list(x.split()) for x in line.strip().split('  ')]
+            words = list(map(de_morse, words_list))
+            print(' '.join(words))
